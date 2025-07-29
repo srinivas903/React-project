1f44b2576e8ae95122ca881b58d2649bf40fc5d3
srinivas903 <2411cs010464@mallareddyuniversity.ac.in>
2025-07-29 10:10:04 +0530

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-// import CulturistApp from './Culturist.jsx'
-// import App from './Culturist.jsx'
-// import LandingPage from './LandingPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
